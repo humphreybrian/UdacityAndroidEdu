b4ef09df3807bb37e3019387764332b2576324ce
Humphrey <humphreybrian43@gmail.com>
2018-05-12 22:54:13 +0300

    Adding Menus and Toast MEssage

--- GithubQuerry/app/src/main/java/com/venturezhub/humphreybrian/githubquerry/MainActivity.java
+++ GithubQuerry/app/src/main/java/com/venturezhub/humphreybrian/githubquerry/MainActivity.java
@@ -1,9 +1,13 @@
 package com.venturezhub.humphreybrian.githubquerry;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +22,32 @@ public class MainActivity extends AppCompatActivity {
         searchUrl = (TextView) findViewById(R.id.tv_url);
         githubResuts = (TextView) findViewById(R.id.tv_github_search_results_json);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_search) {
+            Context context = MainActivity.this;
+            String textToShow = "Search clicked";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //    @Override
+//    public boolean onCreateOptionMenu(Menu menu){
+//
+//        getMenuInflater().inflate(R.menu.main_menu, menu);
+//        return  true;
+//    }
     //TODO Add the EdittextView and reffer it to the id in the activity_main xml file.
 
 }
